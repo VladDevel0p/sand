9f8595f717d42d8863b9647fc20a4fde3d4287e7
vladislavvolcek <vlad.volchek01@gmail.com>
2025-03-01 01:58:53 +0300

    menu for dana -2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,51 @@ import {Dishes, GRAVII, PGS, SAND} from "../utils/consts";
 import {store} from "../store/store";
 import {Provider} from "react-redux";
 import {ButtonTest} from "../components/test/button";
-import {CardImg, MenuCard, MenuList, PageContainer} from "./styles";
 import {useState} from "react";
+import {css, styled} from "@mui/material";
+
+export const PageContainer = styled('div')`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  overflow: auto;
+  gap: 32px;
+`;
+export const MenuList = styled('div')`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 16px;
+  overflow: auto;
+  height: calc(100vh - 200px);
+`;
+
+export const MenuCard = styled('div')<{
+    selected?: boolean;
+}>`
+  width: 300px;
+  height: 400px;
+  border: 2px solid black;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  ${({ selected, theme }) =>
+    selected &&
+    css`
+            border: 3px solid #94cc3a;
+          `}
+`;
+
+export const CardImg = styled('img')`
+  width: 200px;
+  height: 300px;
+  object-fit: contain;
+`;
+
 
 const IndexPage: React.FC<PageProps> = () => {
     const [items,setItems] = useState<string[]>([]);
